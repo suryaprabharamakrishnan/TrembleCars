9461642401b4c3622d0a7599316579012ef50e4a
suryaprabhark <155421858+suryaprabhark@users.noreply.github.com>
2025-05-01 09:15:25 +0530

    Initial Commit
    Project created

---
+++ trimbleCarsBackend/src/main/java/com/trimbleCars/TrimbleCarsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trimbleCars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrimbleCarsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrimbleCarsBackendApplication.class, args);
+	}
+
+}
---
+++ trimbleCarsBackend/src/test/java/com/trimbleCars/TrimbleCarsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trimbleCars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrimbleCarsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
